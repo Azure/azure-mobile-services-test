bde150984437287230e18b2fa14d0b015e81cea8
Dale Anderson <dalander@microsoft.com>
2015-04-17 15:53:20 -0700

    Obtain real channel URI

--- sdk/JavaScript/TestFramework/js/tests/allTests.js
+++ sdk/JavaScript/TestFramework/js/tests/allTests.js
@@ -45,7 +45,9 @@
     zumo.testGroups.push(new zumo.Group(zumo.tests.updateDelete.name, zumo.tests.updateDelete.tests));
     zumo.testGroups.push(new zumo.Group(zumo.tests.login.name, zumo.tests.login.tests));
     zumo.testGroups.push(new zumo.Group(zumo.tests.misc.name, zumo.tests.misc.tests));
-    zumo.testGroups.push(new zumo.Group(zumo.tests.push.name, zumo.tests.push.tests));
+    if (!testPlatform.IsHTMLApplication) {
+        zumo.testGroups.push(new zumo.Group(zumo.tests.push.name, zumo.tests.push.tests));
+    }
     zumo.testGroups.push(new zumo.Group(zumo.tests.api.name, zumo.tests.api.tests));
 
     var allTests = [];
