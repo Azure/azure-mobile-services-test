51cfa508ea16e0ad7db859f0c767e3ded94d2338
Pragna Gopa <pragna.gopa@hotmail.com>
2015-05-12 11:13:31 -0700

    Formatting

--- Runtime/DotNetE2EServerApp/DataObjects/OfflineReady.cs
+++ Runtime/DotNetE2EServerApp/DataObjects/OfflineReady.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System;
 using Microsoft.WindowsAzure.Mobile.Service;
 
 namespace ZumoE2EServerApp.DataObjects
